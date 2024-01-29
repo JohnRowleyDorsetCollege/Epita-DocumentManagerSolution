18e81ac14c03fbc4434ef3dc2175e9e925ebaa47
johnrowley <john.rowley@dorset.ie>
2024-01-29 13:54:41 +0000

    Add project files.

---
+++ DocumentManager.Domain/DocumentManager.cs
@@ -0,0 +1,13 @@
+﻿namespace DocumentManager.Domain
+{
+    public class DocumentItem
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+
+    public class DocumentManager
+    {
+
+    }
+}
---
+++ DocumentManager.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace DocumentManager.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+    }
+}
